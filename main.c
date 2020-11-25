1822331f5a158a043e19b193b1cd9a024cbdb373
Luis <pereiravazluis@gmail.com>
2020-11-25 16:45:34 +0100

    Added Menu (not complet)

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "traitement.h"
+
 
 int main() {
-    printf("Hello, World!\n");
+
+    char scan;
+    char **files;
+
+    do {
+        system("cls||clear");
+        printf("Veuillez choisir une option :\n\n"
+               "- Traiter un fichier (1)\n\n"
+               "- Quitter le programme (0)\n\n"
+               "Votre choix : ");
+        scanf("%c", &scan); //on récup qu'un charactere comme ça c'est plus optimiser
+        fflush(stdin);
+
+        if (scan == '1'){
+            files = takeNameFiles();
+            system("cls||clear");
+            printf("file 1 : %s\n file 2 : %s\n", files[0], files[1]);
+
+            // TODO: Faire traitement pour retrouver les données de chaque fichier
+
+
+        }
+    } while (scan != '0');
+
     return 0;
 }
---
+++ traitement.h
@@ -0,0 +1,6 @@
+#ifndef PISCINEC_TRAITEMENT_H
+#define PISCINEC_TRAITEMENT_H
+
+#endif //PISCINEC_TRAITEMENT_H
+
+#include "traitement.c"
