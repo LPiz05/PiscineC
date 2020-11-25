dadf947146d9731918557e0d18497297cfa13000
gabriel23232 <38659256+gabriel23232@users.noreply.github.com>
2020-11-25 20:55:24 +0100

    Update main.c
    Ajout de la gestion des attribut

--- main.c
+++ main.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct element element;
+typedef struct Tag Tag;
 typedef struct Attr Attr;
 
-struct element{
-    char* open; // balise ouvrante 
+struct Tag{
+    char* start; // balise ouvrante + parametre)
     char* content;
     char* close; // balise de fermeture
-    int* parent; // adresse de la structure du parent ( question 3)
-    int* attributsAmount; //taille du tableau
+    int* childTags ; // adresse de la structure du parent ( question 3)
+    int* childsAmount; //taille du tableau
     struct Attr ** attributs;
 };
 
@@ -20,21 +20,21 @@ struct Attr {
 } ;
 
 void fermetureFichier(FILE*);
-void baliseOuvrante(FILE * ,element*); // lecture de la balise ouvrante
-void baliseFermante(FILE * ,element*);
-void lecturecontent(FILE *, element*); // lecture du content entre 2 balises
-void decoupeAttribut(FILE *,char*, element*,int ); // découper le nom attribut et valeur attribut
-void lectureFichier(FILE* , element*); // fonction qui regroupe toute les autres f° de lecture
-void AfficherFichier(element*); 
+void baliseOuvrante(FILE * ,Tag*); // lecture de la balise ouvrante
+void baliseFermante(FILE * ,Tag*);
+void lecturecontent(FILE *, Tag*); // lecture du content entre 2 balises
+void decoupeAttribut(FILE *,char*, Tag*,int ); // découper le nom attribut et valeur attribut
+void lectureFichier(FILE* ,  Tag*); // fonction qui regroupe toute les autre f° de lecture
+void AfficherFichier(Tag*); // uniquement pour Tester
 
 void fermetureFichier(FILE* f){
     fclose(f);
     printf(" \n Fermeture du fichier!\n");
 }
 
-void baliseOuvrante(FILE *f, element *tab){ // detection de la balise ouvrante et stockage du nom de la balise
+void baliseOuvrante(FILE *f, Tag *tab){ // detection de la balise ouvrante et stockage du nom de la balise
     char a = (char)fgetc(f);
-    char nomBalise[50];
+    char nomBalise[50]; // variable temporaire de stockage d'information
     int compteur = 0;
     int posedeAtt=0; // 0 si faux, 1 si vrai
     while ( a != '<'){ // recherche balise d'ouverture
@@ -53,23 +53,23 @@ void baliseOuvrante(FILE *f, element *tab){ // detection de la balise ouvrante e
         }
         if(posedeAtt==1)
         {
-            tab->open = malloc(sizeof(char )* strlen(nomBalise));
-            strcpy(tab->open ,nomBalise);
+            tab->start = malloc(sizeof(char )* strlen(nomBalise));
+            strcpy(tab->start ,nomBalise);
             compteur = 0;
             posedeAtt = 2;
         }
     }
     nomBalise[compteur] = '\0';
      if( posedeAtt==0){
-         tab->open = malloc(sizeof(char )* strlen(nomBalise));
-         strcpy(tab->open ,nomBalise);
+         tab->start = malloc(sizeof(char )* strlen(nomBalise));
+         strcpy(tab->start ,nomBalise);
      }else{
          char * tempo = &nomBalise;
-         //decoupeAttribut(f,tempo, tab, compteur);
+         decoupeAttribut(f,tempo, tab, compteur);
      }
 }
 
-void lecturecontent(FILE *f, element *tab){ // lecture des donnée jusque detection de la balise de fermeture ( </ )
+void lecturecontent(FILE *f, Tag *tab){ // lecture des donnée jusque detection de la balise de fermeture ( </ )
     char a = (char)fgetc(f);
     char content[30]; // variable temporaire de stockage d'information
     int i =0;
@@ -90,12 +90,13 @@ void lecturecontent(FILE *f, element *tab){ // lecture des donnée jusque detect
 
 }
 
-void decoupeAttribut(FILE *f, char *nomBalise ,element *tab, int longeur) { // NE FONCTIONNE PAS
+void decoupeAttribut(FILE *f, char *nomBalise ,Tag *tab, int longeur) {
     char nomAtt[30];
     char valeurAtt[30];
     int compteur = 0;
     int nombreAtt = 0;
     Attr tempo[10];
+
     int i = 0;
     int fin = 0;
     while (fin == 0|| i<longeur){
@@ -104,12 +105,11 @@ void decoupeAttribut(FILE *f, char *nomBalise ,element *tab, int longeur) { // N
                 fin = 1;
             } else {
                 i++;
-                if(nomBalise[i] != '='){
-                    nomAtt[compteur] = nomBalise[i];
-                    compteur++;
-                }
+                nomAtt[compteur] = nomBalise[i];
+                compteur++;
             }
         }
+        nomAtt[compteur-1]='\0';
         compteur = 0;
         while (nomBalise[i] != ' ') {
             if (nomBalise[i] == '\0' || nomBalise[i]=='>') {
@@ -121,26 +121,30 @@ void decoupeAttribut(FILE *f, char *nomBalise ,element *tab, int longeur) { // N
                 compteur++;
             }
         }
+        valeurAtt[compteur]='\0';
         compteur = 0;
         struct Attr monAttr;
-        monAttr.name = nomAtt;
-        monAttr.value = valeurAtt;
-        tempo[nombreAtt] = monAttr;
-        nombreAtt++;
-    }
+       // monAttr.name = nomAtt;
+        //monAttr.value = valeurAtt;
 
-    tab->attributs = malloc(sizeof( Attr )* nombreAtt); //nombreAtt = nombre d'attribut
-    for (int i=0; i<nombreAtt;i++)
-    {
-        Attr  *att = &tempo[i];
-        strcpy(tab->attributs[nombreAtt] , att);
+        char *test = nomAtt;
+        char  *teeest = valeurAtt;
+        tempo[nombreAtt].name= (char *) test;
+        tempo[nombreAtt].value= (char *) teeest;
+        nombreAtt++;
+
     }
-    tab->attributsAmount = malloc(sizeof(int* )* nombreAtt);
-    tab->attributsAmount= nombreAtt;
+//
+    tab->attributs = malloc(sizeof( Attr )* nombreAtt);
+    for (int y=0; y<nombreAtt;y++)
+    {
+        tab->attributs[y] = &tempo[y];
+    }
+    //tab->attributsAmount= nombreAtt;
 }
 
 
-void baliseFermante(FILE * f,element* tab){;
+void baliseFermante(FILE * f,Tag* tab){;
     char nomBalise[30];
     int compteur = 0;
     char c = (char)fgetc(f);
@@ -155,16 +159,16 @@ void baliseFermante(FILE * f,element* tab){;
 }
 
 
-void lectureFichier(FILE* f, element *tab){
+void lectureFichier(FILE* f, Tag *tab){
 
     baliseOuvrante(f,tab);
     lecturecontent(f,tab);
     baliseFermante(f,tab);
 }
 
-void AfficherFichier(element *tab)
+void AfficherFichier(Tag *tab)
 {
-    printf(" \n %s", tab->open);
+    printf(" \n %s", tab->start);
     printf(" \n %s", tab->content);
     printf(" \n %s", tab->close);
 }
@@ -174,32 +178,40 @@ int main() {
     int fin = 0;
     int* finFichier = &fin;
 
-    element rien;
-    element *test = &rien; // tableau de struct
+    /*Tag rien;
+    Tag *test = &rien; // tableau de struct*/
+
     FILE* f = fopen("test.txt","r");
-    element tab[10];
+    Tag tab[10];
     int index = 0; // represente les index du tableau de struct
-    element *pointeur = &tab[0];
+    Tag *pointeur = &tab[0];
     if (f!= NULL){
         printf("Le fichier est ouvert !\n");
     }else{
         printf("Probleme de l'ouverture du fichier!\n");
         return 0;
     }
+
     lectureFichier(f,pointeur);
     AfficherFichier(pointeur);
-    char c = (char)fgetc(f);
+    char c = ' ';
+    //char c = (char)fgetc(f);
     int ligne = 0;
-    while (c != EOF) 
+    while (c != EOF)
     {
-        while ( c  != '\n') // sup les caractère en trop hors des balises
+
+        c = (char)fgetc(f);
+       /* while ( c != EOF)
         {
             c = (char)fgetc(f);
-        }
-            ligne ++;
-            pointeur = &tab[ligne];
-            lectureFichier(f,pointeur);
-            AfficherFichier(pointeur);
+        }*/
+    if ( c == '\n')
+    {
+        ligne ++;
+        pointeur = &tab[ligne];
+        lectureFichier(f,pointeur);
+        AfficherFichier(pointeur);
+    }
 
     }
     fermetureFichier(f);
